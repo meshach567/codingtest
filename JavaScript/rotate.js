476312022ce25879c3f12653f3c7c7c259fb60a9
meshach567 <arinzemeshach123@gmail.com>
2024-10-09 09:58:36 +0100

    commit

--- JavaScript/rotate.js
+++ JavaScript/rotate.js
@@ -10,11 +10,11 @@ function rotateArray(arr, d) {
 
 
     // Copy last n - d elements to the front of temp
-    for (let i = 0; i < n - d; i++)
+    for(let i = 0; i < n - d; i++)
         temp[i] = arr[d + i];
 
-    for (let i = 0; i < d; i++)
-        temp[n - d + i] = arr[i];
+    for(let i = 0; i < d; i++)
+        temp[n-d+i] = arr[i];
 
     for (let i = 0; i < n; i++)
         arr[i] = temp[i];
