6510677645a7cd28054d12274251d61b5f65776f
meshach567 <arinzemeshach123@gmail.com>
2024-10-10 21:59:02 +0100

    commit

--- JavaScript/rightrotate.js
+++ JavaScript/rightrotate.js
@@ -1,21 +1,20 @@
-function rotateRight(arr, k) {
-    let n = arr.length;
+function rotateArray(arr, k) {
+    const n = arr.length;
 
     k = k % n;
 
     for (let i = 0; i < n; i++) {
-        if (i < k) { 
-            process.stdout.write(arr[n + i - k] + " ") 
-        } else { 
-            process.stdout.write(arr[i - k] + " ")
+        if(i < k) {
+            process.stdout.write(arr[ n + i - k] + " ");
+        } else {
+            process.stdout.write(arr[ i - k] + " ");
         }
     }
 
     process.stdout.write("\n");
 }
 
-const Array = [4,5,6,7,8,9];
-
+const Array = [5,6,7,8,9,0];
 const k = 3;
 
-rotateRight(Array, k);
+rotateArray(Array, k)
