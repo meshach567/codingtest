236a65cab6b6ed814414fc32584fcfebe1528fb1
meshach567 <arinzemeshach123@gmail.com>
2024-11-14 15:18:20 +0100

    quicksort_array

--- python/quicksort_array.py
+++ python/quicksort_array.py
@@ -1,3 +1,15 @@
+# How it works:
+# Choose a value in the array to be the pivot element.
+# Order the rest of the array so that lower values than the pivot element are on the left, and higher values are on the right.
+# Swap the pivot element with the first element of the higher values so that the pivot element lands in between the lower and higher values
+# Do the same operations (recursively) for the sub-arrays on the left and right side of the pivot element
+
+# An array with values to sort.
+# A quickSort method that calls itself (recursion) if the sub-array has a size larger than 1.
+# A partition method that receives a sub-array, moves values around, swaps the pivot element into the sub-array and returns the index where the 
+# next split in sub-arrays happens.
+#
+
 def partition(array, low, high):
     pivot = array[high]
     i = low - 1
